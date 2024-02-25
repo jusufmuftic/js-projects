6ff57829be6cdf964b70bff77441bc8bb1d4bd9e
Jusuf <148939814+jusufmuftic@users.noreply.github.com>
2024-02-25 14:05:43 +0100

    script.js

--- projects/shopping-cart/script.js
+++ projects/shopping-cart/script.js
@@ -1,28 +1,29 @@
-let allTotal = 0;
+let allTotal = 0
 
-function addToCart(btn) {
-    let mainEl = btn.closest('.single-item')
-    let price = mainEl.querySelector('.price').innerText
+function addToCart(e) {
+    let mainEl = e.closest('.single-item')
     let name = mainEl.querySelector('h3').innerText
+    let price = mainEl.querySelector('.price').innerText
     let quantity = mainEl.querySelector('input').value
     let cartItems = document.querySelector('.cart-items')
     let vegies = document.querySelectorAll('.single-item')
 
-    if (parseInt(quantity) > 0) {
+    if(parseInt(quantity) > 0) {
         price = price.substring(1)
         price = parseInt(price)
-        quantity = parseInt(quantity)
-        let total = price * quantity
 
+        let total = price * parseInt(quantity)
         allTotal += total
-
+        
+        
+        
         cartItems.innerHTML += `<div class="cart-single-item">
-                                    <h3>${name}</h3>
-                                    <p>$${price} x ${quantity} = $<span>${total}</span></p>
-                                    <button onclick="removeFromCart(this) "class="remove-item">Remove</button>
+                                <h3>${name}</h3>
+                                <p>$${price} x ${quantity} = $<span>${total}</span><p>
+                                <button onclick="removeItem(this)">Remove</button>
                                 </div>`
 
-        document.querySelector('.total').innerText = `Total: $${allTotal}`
+        document.querySelector('.total').innerHTML = `<b>Total:</b> $${allTotal}`
 
         vegies.forEach((vegie) => {
             let itemName = vegie.querySelector('.si-content h3').innerText
@@ -33,26 +34,26 @@ function addToCart(btn) {
     } else {
         alert('Choose a quantity')
     }
-}
+    
+}   
 
-function removeFromCart(btn) {
-    let mainEl = btn.closest('.cart-single-item')
-    let price = mainEl.querySelector('p span').innerText
+function removeItem(e) {
+    let mainEl = e.closest('.cart-single-item')
     let name = mainEl.querySelector('h3').innerText
     let vegies = document.querySelectorAll('.single-item')
+    let price = mainEl.querySelector('p span').innerText
+    
     price = parseInt(price)
-
     allTotal -= price
 
-    document.querySelector('.total').innerText = `Total: $${allTotal}`
+    document.querySelector('.total').innerHTML = `<b>Total:</b> $${allTotal}`
+
     mainEl.remove()
 
-    vegies.forEach((vegie)=> {
+    vegies.forEach((vegie) => {
         let itemName = vegie.querySelector('.si-content h3').innerText
         if(itemName === name) {
             vegie.querySelector('.actions input').value = 0
-            vegie.querySelector('.actions button').removeAttribute('disabled')
-            vegie.querySelector('.actions button').innerText = 'Add'
-        }
+        } 
     })
 }
